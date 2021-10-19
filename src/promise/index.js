484867d81911818215647d2d6fbf2b3e03a9be0b
wrsmith10 <richardsmithortegon@gmail.com>
2021-10-19 16:29:51 -0500

    resolver el reto con promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -12,3 +12,29 @@ const SomethingWillHappen=()=>{
 SomethingWillHappen()
 .then(response=>console.log(response))
 .catch(err=> console.error(err));
+
+const somethingWillHappen2=()=>{
+    return new Promise((resolve,reject) =>{
+        if (true){
+            setTimeout(()=>{
+                resolve('True');
+            },2000)
+        }
+        else{
+           const error= new Error('Whoops!') ;
+           reject(error);
+        }
+    })
+}
+
+somethingWillHappen2()
+.then(response =>console.log(response))
+.catch(err=>console.error(err));
+
+Promise.all([SomethingWillHappen(),somethingWillHappen2()])
+.then(response=>{
+    console.log('Array of results', response);
+})
+.catch(err=>{
+    console.error(err);
+})
